baab2b6d9e735baa2d7cc1eb3109dabb2a91b8ab
LAPTOP-36SKAH06\marla <marlenalukomska@wp.pl>
2019-04-27 11:10:55 +0200

    Main page with links to converters done

--- src/main/java/pl/net/rogala/converter/MainController.java
+++ src/main/java/pl/net/rogala/converter/MainController.java
@@ -1,7 +1,12 @@
 package pl.net.rogala.converter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping("/")
+    public String showMainPage() {
+        return "temperature/mainPage";
+    }
 }
